c2a22e76eda0bc89bdccf375432c9583fafed460
pozafly <pozafly@gmail.com>
2024-03-01 11:26:51 +0900

    refactor: app.ts 코드 정리

--- src/app.ts
+++ src/app.ts
@@ -5,23 +5,25 @@ import { commandOpenModal } from './functions/commandOpenModal';
 import { insertAllUsers } from './functions/insertAllUsers';
 import { responseModal } from './functions/responseModal';
 
+// init
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
-app.message('hello', async ({ message, say }) => {
-  console.log('message', message);
-  console.log('say', say);
-});
-
+/**
+ * event 등록
+ * shortcut: 단축키 실행
+ * action : 모달에서 '전체 선택' 버튼 클릭 시
+ * view: 모달에서 '제출' 버튼 클릭 시
+ */
+//
 app.shortcut<GlobalShortcut>('random', openModal);
 app.command('/random', commandOpenModal);
-
-// https://api.slack.com/methods/views.update
 app.action('insert_all_users', insertAllUsers);
 app.view<ViewSubmitAction>('random', responseModal);
 
+// start
 (async () => {
   await app.start(process.env.PORT || 3000);
   console.log('⚡️ Bolt app is running!');
